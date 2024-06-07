09d5aeb5e325cd539adcd2ce0da4a8136429477e
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-06-07 13:53:55 +0600

    07 6 1

---
+++ overload_2.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+    void print(int a){
+    cout<<"Int-->"<<a<<endl;
+    }
+
+    void print(double a){
+    cout<<"Double-->"<<a<<endl;
+    }
+
+    void print(char a, char b){
+    cout<<"Char-->"<<a<<endl;
+    }
+
+    void print(int a, double b){
+    cout<<"Sum-->"<<a+b<<endl;
+    }
+
+    void print(int a, int b){
+    cout<<"Sum-->"<<a+b<<endl;
+    }
+
+    void print(){
+    cout<<"No data"<<endl;
+    }
+
+
+int main()
+{
+    print(6,5.6);
+    return 0;
+}
+
